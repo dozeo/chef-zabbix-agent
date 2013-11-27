7e6e5d2980f23abe9361667424a2eb4af57b6ddf
Andreas Jaekle <andreas@jaekle.net>
2013-11-27 14:12:50 +0100

    changed mdraid to work better

--- recipes/mdraid.rb
+++ recipes/mdraid.rb
@@ -10,30 +10,12 @@ directory "/opt/chef/zabbix/" do
   recursive true
 end
 
-cookbook_file "/opt/chef/zabbix/check_md_raid" do
-  owner "zabbix"
-  group "zabbix"
-  mode 0755
-  source "check_md_raid"
-end
-
-file "/etc/zabbix/agent-conf.d/md.conf" do
-  content %Q{UserParameter=dev.md[*],/usr/bin/sudo /opt/chef/zabbix/check_md_raid -d /dev/md$1}
+cookbook_file "/etc/zabbix/agent-conf.d/mdadm.conf" do
   owner "root"
   group "root"
   mode 0644
+  source "mdadm.conf"
   notifies :restart, "service[#{node['zabbix']['service']}]", :delayed
-end
-
-begin
-  sudo "zabbix-mdadm" do
-    user "zabbix"
-    commands ['/opt/chef/zabbix/check_md_raid -d /dev/md[0-9]']
-    nopasswd true
-  end
-rescue NameError => e
-  Chef::Log.info(e.message)
-  Chef::Log.info("LWRP for sudo is not defined. Skipping resource sudo[zabbix-mdadm] ...")
 end
 
 # vim: ts=2 sw=2 et ai
